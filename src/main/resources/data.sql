fbf8cdff100df0bdfb832a236a351d38157d4842
Noikedan <noikedan00@gmail.com>
2021-06-24 23:18:37 +0900

    Docker+Postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-insert into tasks(status, title, lmt) values('f', 'シュート練習',  CURRENT_DATE);
-insert into tasks(status, title, lmt) values('f', 'ドリブル練習',  CURRENT_DATE);
+INSERT INTO tasks(id, status, title, lmt) values(nextval('tasks_seq'), 'f', 'シュート練習', CURRENT_DATE);
+INSERT INTO tasks(id, status, title, lmt) values(nextval('tasks_seq'), 'f', 'ドリブル練習', CURRENT_DATE);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,10 @@
-CREATE TABLE tasks 
-(
-　id SERIAL,
-　status BOOLEAN NOT NULL,
-　title  VARCHAR(100) NOT NULL,
-　lmt DATE NOT NULL
+CREATE SEQUENCE tasks_seq;
+
+CREATE TABLE tasks (
+    id integer NOT NULL DEFAULT nextval('tasks_seq'),
+	status BOOLEAN NOT NULL,
+	title  VARCHAR(100) NOT NULL,
+	lmt DATE NOT NULL
 );
+ALTER SEQUENCE tasks_seq 
+OWNED BY tasks.id
